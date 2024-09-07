8ff0df13e1b806e6bbf999d0226c728eca8e5872
to4to <justjustifying@outlook.com>
2024-09-08 01:08:24 +0530

    feat: Add logic for handling different URL paths
    - Added a new logic handler function in the middleware package to handle different URL paths.
    - The logic handler uses the `switch` statement to determine the appropriate action based on the requested URL path.
    - The logic handler currently handles the "/restricted" and "/login" paths, with support for GET and POST methods.

--- server/middleware/middleware.go
+++ server/middleware/middleware.go
@@ -6,17 +6,21 @@ import (
 	"github.com/justinas/alice"
 )
 
+func NewHandler() http.Handler {
 
-
-func NewHandler()http.Handler{
-
-return alice.New(recoverHandler,authHandler).ThenFunc(logicHandler)
+	return alice.New(recoverHandler, authHandler).ThenFunc(logicHandler)
 
 }
 
+func logicHandler(w http.ResponseWriter, r *http.Request) {
 
-func logicHandler(w http.ResponseWriter,r *http.Request){
-
-
-	switch r.URL.Path{}
+	switch r.URL.Path {
+	case "/restricted":
+	case "/login":
+		switch r.Method {
+		case "GET":
+		case "POST":
+		default:
+		}
+	}
 }
